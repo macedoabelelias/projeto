3f26f932dabd358da309643976fbeae6818e1f98
abelmacedo <macedoabel.elias@gmail.com>
2025-03-07 11:28:30 -0300

    restrição de usuário inativo - aula 49

--- autenticar.php
+++ autenticar.php
@@ -13,9 +13,15 @@ $res = $query->fetchAll(PDO::FETCH_ASSOC);
 $linhas = @count($res);
 
 if($linhas > 0){
+
+    if($res[0]['ativo'] != 'Sim'){
+        echo '<script>window.alert("Acesso destivado!")</script>';
+        echo '<script>window.location="index.php"</script>'; 
+    }
     $_SESSION['nome'] = $res[0]['nome'];
     $_SESSION['id'] = $res[0]['id'];
     $_SESSION['nivel'] = $res[0]['nivel'];
+
     
     echo '<script>window.location="painel"</script>';
 }else{
