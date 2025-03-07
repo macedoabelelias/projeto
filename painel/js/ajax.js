260e4b7f37d2a25e6f5d8d5e54b108f4ad671ad7
abelmacedo <macedoabel.elias@gmail.com>
2025-03-07 16:22:00 -0300

    ajax para salvar - aula 53

--- painel/js/ajax.js
+++ painel/js/ajax.js
@@ -16,52 +16,47 @@ function listar(){
     });
 }
 
-// function inserir(){
-//     $('#id_usuario').val(localStorage.id_usu);
-//     $('#id_empresa').val(localStorage.id_empresa);
-//     $('#mensagem').text('');
-//     $('#titulo_inserir').text('Inserir Registro');
-//     $('#modalForm').modal('show');
-//     limparCampos();
-// }
+function inserir(){
+    $('#mensagem').text('');
+    $('#titulo_inserir').text('Inserir Registro');
+    $('#modalForm').modal('show');
+    limparCampos();
+}
+
+$("#form").submit(function () {
+
+    event.preventDefault();
+    var formData = new FormData(this);
+
+    $.ajax({
+        url: 'paginas/' + pag + "/salvar.php",
+        type: 'POST',
+        data: formData,
+
+        success: function (mensagem) {
+            $('#mensagem').text('');
+            $('#mensagem').removeClass()
+            if (mensagem.trim() == "Salvo com Sucesso") {
+
+                $('#btn-fechar').click();
+                listar();          
+
+            } else {
+
+                $('#mensagem').addClass('text-danger')
+                $('#mensagem').text(mensagem)
+            }
 
 
+        },
 
+        cache: false,
+        contentType: false,
+        processData: false,
 
-// $("#form").submit(function () {
+    });
 
-//     event.preventDefault();
-//     var formData = new FormData(this);
-
-//     $.ajax({
-//         url: 'paginas/' + pag + "/salvar.php",
-//         type: 'POST',
-//         data: formData,
-
-//         success: function (mensagem) {
-//             $('#mensagem').text('');
-//             $('#mensagem').removeClass()
-//             if (mensagem.trim() == "Salvo com Sucesso") {
-
-//                 $('#btn-fechar').click();
-//                 listar();          
-
-//             } else {
-
-//                 $('#mensagem').addClass('text-danger')
-//                 $('#mensagem').text(mensagem)
-//             }
-
-
-//         },
-
-//         cache: false,
-//         contentType: false,
-//         processData: false,
-
-//     });
-
-// });
+});
 
 
 
