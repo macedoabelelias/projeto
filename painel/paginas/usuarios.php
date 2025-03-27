105f61d4338974c8b654135d4b19c9a2bef1febf
abelmacedo <macedoabel.elias@gmail.com>
2025-03-27 16:16:15 -0300

    página de usuários -inicio - aula39

---
+++ painel/paginas/usuarios.php
@@ -0,0 +1,10 @@
+<?php
+$pag = 'usuarios';
+?>
+
+<a type="button" class="btn btn-primary"><span class="fa fa-plus"></span> Usuário</a>
+
+<div class="bs-example widget-shadow" style="padding:15px" id="listar">
+   
+
+</div>
